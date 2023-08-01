7035ac423d4c7f2901897b1c431628abc3dc9373
hammadkhan96 <mhk99982@gmail.com>
2023-08-02 02:53:12 +0500

    add exception handling

--- app/jobs/get_chatgpt_response.rb
+++ app/jobs/get_chatgpt_response.rb
@@ -11,14 +11,19 @@ class GetChatgptResponse
   private
 
   def call_openai(chat_room)
-    @response = OpenAI::Client.new.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo',
-        messages: Conversation.for_openai(chat_room.conversations),
-        temperature: 0.1
-      }
-    )
-
-    Conversation.create!(chat_room_id: chat_room.id, role: 'assistant', message: @response.dig('choices', 0, 'message', 'content'))
+    begin
+      @response = OpenAI::Client.new.chat(
+        parameters: {
+          model: 'gpt-3.5-turbo',
+          messages: Conversation.for_openai(chat_room.conversations),
+          temperature: 0.1
+        }
+      )
+  
+      Conversation.create!(chat_room_id: chat_room.id, role: 'assistant', message: @response.dig('choices', 0, 'message', 'content'))
+  
+    rescue StandardError => e
+      puts "Error occurred: #{e.message}"
+    end
   end
 end
