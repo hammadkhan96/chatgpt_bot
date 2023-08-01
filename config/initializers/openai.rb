c9dcc0028574010cd24e5ffc8f992d749c8230f4
hammadkhan96 <mhk99982@gmail.com>
2023-08-02 02:23:11 +0500

    remove logger

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openAI.access_token
+  config.access_token = "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"
 end
